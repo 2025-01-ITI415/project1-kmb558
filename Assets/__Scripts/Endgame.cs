dd6d2d24e8f72a5237b12a37205ef31d0f570dc0
kmb558 <kmb558@scarletmail.rutgers.edu>
2025-02-27 08:56:36 -0500

    Goal Point
    Trying to bring in goal point from mission

---
+++ Assets/__Scripts/Endgame.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Renderer))]
+public class Goal : MonoBehaviour
+{
+	static public bool goalMet = false;
+
+	void OnTriggerEnter(Collider other)
+	{
+		// when the trigger is hit by something
+		// check to see if it's a Attacker
+		Attacker proj = other.GetComponent<Attacker>();
+		if (proj != null)
+		{
+			// if so, set goalMet = true
+			Goal.goalMet = true;
+
+			// also set the alpha of the color of higher opacity
+			Material mat = GetComponent<Renderer>().material;
+			Color c = mat.color;
+			c.a = 0.75f;
+			mat.color = c;
+		}
+	}
+}
